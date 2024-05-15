25e551c2c33e5b08e301babaff1c64dabef73eb4
johannes ryberg <johannes@qualitythink.se>
2024-05-15 23:54:43 +0200

    <Board> component has its own state that takes in the data from the parent 'App.tsx' and i also gave the elements <li> and <button> some new properties

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,23 +1,29 @@
-import { TileProps } from "../app/validation"
+import { useEffect, useState } from "react";
+import { TileProps } from "../app/validation.ts";
 
 type BoardProps = {
 	tileList: TileProps[]
 }
 
 const Board = ( { tileList } : BoardProps ) => {
+	const [modifiedTileList, setModifiedTileList] = useState<TileProps[]>(tileList)
+
+	useEffect(() => {
+        setModifiedTileList(tileList);
+    }, [tileList]);
+
 	return (
 		<ul className="board">
-			{
-				tileList.map( property => (
-					<li className="board__tile" style={{ gridColumn: property.column, gridRow: property.row }}>
-						<button>
-							{property.number}
-						</button>
-					</li>
-				))
-			}
+			{modifiedTileList.map( property => (
+				<li key={property.number} className="board__tile" style={{ gridColumn: property.column, gridRow: property.row }}>
+					<button disabled={property.disabled}>
+						{property.number}
+					</button>
+				</li>
+			))}
 		</ul>
 	)
 }
+
 
 export default Board
